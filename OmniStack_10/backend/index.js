9d3506c9be7f75fd5260c3aaae7eec73d1d75169
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-14 20:39:33 -0300

    Add mongoose - comando: yarn add mongoose

--- OmniStack_10/backend/index.js
+++ OmniStack_10/backend/index.js
@@ -12,6 +12,8 @@ app.use( express.json() ) // Configurando express para atender requisições que
 // Route Params: request.params ( Identificar um recurso na alteração ou remoção )
 // Body: request.body ( Dados para criação ou alteração de um registro )
 
+// MongoDB ( Não-Relacional )
+
 app.post( '/users', ( request, response ) => {
     console.log( request.body )
     return response.json( { message: "Hello OmniStack!!!" } )
