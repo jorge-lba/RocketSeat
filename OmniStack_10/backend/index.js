a7de69056443032fa8e031fd3d6ee03e8c4c96e0
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-14 20:47:08 -0300

    Importando e Confogurando mongoose

--- OmniStack_10/backend/index.js
+++ OmniStack_10/backend/index.js
@@ -1,6 +1,14 @@
 const express = require( 'express' )
+const mongoose = require( 'mongoose' )
 
 const app = express()
+
+mongoose.connect( 'mongodb+srv://jorgeomnistack:OmNiSTACK10@reacketseat-glpu1.mongodb.net/week10?retryWrites=true&w=majority', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+} )
 
 app.use( express.json() ) // Configurando express para atender requisições que tem o corpo no formato json
 
