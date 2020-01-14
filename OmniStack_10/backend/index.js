5d0037d6c38e3f943fd6894b6dcd2959b7f3034d
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-14 19:54:25 -0300

    Testando metodo GET, pegando valor do query

--- OmniStack_10/backend/index.js
+++ OmniStack_10/backend/index.js
@@ -2,7 +2,16 @@ const express = require( 'express' )
 
 const app = express()
 
-app.get( '/', ( request, response ) => {
+// Mátodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: req.query ( Filtros, Ordenação, Paginação, ... )
+// Route Params:
+// Body:
+
+app.get( '/users', ( request, response ) => {
+    console.log( request.query )
     return response.json( { message: "Hello OmniStack!!!" } )
 } )
 
