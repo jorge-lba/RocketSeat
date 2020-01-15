eb5b81edf875efddcfe07c80909e6b1dba2ebd29
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-14 22:05:10 -0300

    Tratando string enviada com texto e transformando em array

--- OmniStack_10/backend/src/routes.js
+++ OmniStack_10/backend/src/routes.js
@@ -12,6 +12,10 @@ routes.post( '/devs', async ( request, response ) => {
     const { login, avatar_url, bio } = apiResponse.data
     const name = apiResponse.data.name ? apiResponse.data.name : login
 
+    const techsArray = techs.split( ',' ).map( tech => tech.trim() )
+
+    console.log( techsArray )
+
     return response.json( { message: "Hello OmniStack!!!" } )
 } )
 
