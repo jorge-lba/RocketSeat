e4d0d11f7553b5e41bcf52f1c76d39232de915b3
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-15 00:08:12 -0300

    Implementada a funçao para verificar se o usuario ja existe, caso exista retorna um msg avisando que o usuario ja foi cadastrado, caso nao exista faz o cadastro.

--- OmniStack_10/backend/src/models/controllers/DevController.js
+++ OmniStack_10/backend/src/models/controllers/DevController.js
@@ -6,27 +6,37 @@ module.exports ={
 
         const { github_username, techs, longitude, latitude } = request.body
 
-        const apiResponse = await axios.get( `https://api.github.com/users/${ github_username }` )
+        let dev = await Dev.findOne( { github_username } )
 
-        const { login, avatar_url, bio } = apiResponse.data
-        const name = apiResponse.data.name ? apiResponse.data.name : login
+        if ( !dev ){
+            
+            const apiResponse = await axios.get( `https://api.github.com/users/${ github_username }` )
+    
+            const { login, avatar_url, bio } = apiResponse.data
+            const name = apiResponse.data.name ? apiResponse.data.name : login
+    
+            const techsArray = techs.split( ',' ).map( tech => tech.trim() )
+    
+            const location = {
+                type: 'Point',
+                coordinates: [ longitude, latitude ]
+            }
+    
+                dev = await Dev.create( {
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            } )
+            
+            return response.json( dev )
 
-        const techsArray = techs.split( ',' ).map( tech => tech.trim() )
-
-        const location = {
-            type: 'Point',
-            coordinates: [ longitude, latitude ]
+        } else {
+            return response.json( { message: ` User ${ github_username } is already registered ` } )
         }
 
-        const dev = await Dev.create( {
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location
-        } )
 
-        return response.json( dev )
     }
 }
