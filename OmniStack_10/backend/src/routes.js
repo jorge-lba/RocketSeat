faa27e33359233f723ef318b57d12106df6a433a
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-14 22:01:29 -0300

    Usando axios para consumir API do gitgube, e separando as informaçoes que vamos usar

--- OmniStack_10/backend/src/routes.js
+++ OmniStack_10/backend/src/routes.js
@@ -3,9 +3,14 @@ const axios = require( 'axios' )
 
 const routes = Router()
 
-routes.post( '/devs', ( request, response ) => {
+routes.post( '/devs', async ( request, response ) => {
 
-    const { github_username } = request.body
+    const { github_username, techs } = request.body
+
+    const apiResponse = await axios.get( `https://api.github.com/users/${ github_username }` )
+
+    const { login, avatar_url, bio } = apiResponse.data
+    const name = apiResponse.data.name ? apiResponse.data.name : login
 
     return response.json( { message: "Hello OmniStack!!!" } )
 } )
