ec22619d9b5a8da1fbecd8fc3561a4cce91cb59c
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-14 23:58:45 -0300

    Criando controller DevControle (Removendo metodo do routes para ficar mais organizado )

--- OmniStack_10/backend/src/routes.js
+++ OmniStack_10/backend/src/routes.js
@@ -1,35 +1,8 @@
 const { Router } = require( 'express' )
-const axios = require( 'axios' )
-const Dev = require( './models/Dev.js' )
+const DevController = require( './models/controllers/DevController.js' )
 
 const routes = Router()
 
-routes.post( '/devs', async ( request, response ) => {
-
-    const { github_username, techs, longitude, latitude } = request.body
-
-    const apiResponse = await axios.get( `https://api.github.com/users/${ github_username }` )
-
-    const { login, avatar_url, bio } = apiResponse.data
-    const name = apiResponse.data.name ? apiResponse.data.name : login
-
-    const techsArray = techs.split( ',' ).map( tech => tech.trim() )
-
-    const location = {
-        type: 'Point',
-        coordinates: [ longitude, latitude ]
-    }
-
-    const dev = await Dev.create( {
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location
-    } )
-
-    return response.json( dev )
-} )
+routes.post( '/devs', DevController.store )
 
 module.exports = routes
