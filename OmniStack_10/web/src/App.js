d90a37fa2e677a5b67c306717d0597009de26c15
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-15 22:37:12 -0300

    Utilizando e entendendo as propriedades de um Componente

--- OmniStack_10/web/src/App.js
+++ OmniStack_10/web/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './Header'
 
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
+// Propriedade: Informações que um componete PAI passa para o componente FILHO.
+
 function App() {
   return (
     <>
-      <Header />
+      <Header title="Dashboard" />
       <h1>Hello Word!</h1>
     </>
   );
