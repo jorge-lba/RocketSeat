f682ba8df9cffca5d559be321c321e297c8779e2
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-18 17:39:51 -0300

    Add input para busca

--- OmniStack_10/mobile/src/pages/Main.js
+++ OmniStack_10/mobile/src/pages/Main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Image, View, Text } from 'react-native'
+import { StyleSheet, Image, View, Text, TextInput } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
@@ -38,21 +38,33 @@ function Main ( { navigation } ) {
     }
 
     return (
-        < MapView  initialRegion={ currentRegion } style={ styles.map } >
-            < Marker  coordinate={ { latitude: -23.5255981, longitude: -47.4919055 } }> 
-                <Image style={ styles.avatar } source={ { uri: 'https://avatars2.githubusercontent.com/u/56704254?s=400&v=4' } } />
+        <> 
+            < MapView  initialRegion={ currentRegion } style={ styles.map } >
+                < Marker  coordinate={ { latitude: -23.5255981, longitude: -47.4919055 } }> 
+                    <Image style={ styles.avatar } source={ { uri: 'https://avatars2.githubusercontent.com/u/56704254?s=400&v=4' } } />
 
-                <Callout onPress={ () => {
-                    navigation.navigate( 'Profile', { github_username: 'jorge-lba' } )
-                } }>
-                    <View style={ styles.callout } >
-                        <Text style={ styles.devName }>Jorge Alegretti</Text>
-                        <Text style={ styles.devBio }>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
-                        <Text style={ styles.devTechs }>Javascript, ReactJS, React Native, Node.js</Text>
-                    </View>
-                </Callout>
-            </ Marker>
-        </ MapView>
+                    <Callout onPress={ () => {
+                        navigation.navigate( 'Profile', { github_username: 'jorge-lba' } )
+                    } }>
+                        <View style={ styles.callout } >
+                            <Text style={ styles.devName }>Jorge Alegretti</Text>
+                            <Text style={ styles.devBio }>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
+                            <Text style={ styles.devTechs }>Javascript, ReactJS, React Native, Node.js</Text>
+                        </View>
+                    </Callout>
+                </ Marker>
+            </ MapView>
+            <View style={ styles.searchFrom }>
+                <TextInput 
+                    style={ styles.searchInput }
+                    placeholder= 'Buscar devs por techs...'
+                    placeholderTextColor= '#999'
+                    autoCapitalize= 'words'
+                    autoCorrect={ false }
+                />
+                
+            </View>
+        </>
     )
 }
 
