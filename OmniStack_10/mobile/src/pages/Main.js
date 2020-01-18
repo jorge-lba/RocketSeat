e455482c67d87e666a20f2c27593902a33a16024
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-18 00:14:46 -0300

    Add tag com imagem

--- OmniStack_10/mobile/src/pages/Main.js
+++ OmniStack_10/mobile/src/pages/Main.js
@@ -1,45 +1,62 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import { StyleSheet, Image } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
 function Main ( ) {
     
     const [ currentRegion, setCurrentRegion ] = useState( null )
 
-    useEffect( ( ) => async function loadInitialPosition() {
+    useEffect( ( ) =>{
         
+        async function loadInitialPosition() {
         
-        const { granted } = await requestPermissionsAsync()
+            const { granted } = await requestPermissionsAsync()
 
-        if ( granted ) {
-            const { coords } = await getCurrentPositionAsync( {
-                enableHighAccuracy: true,
-            } )
+            if ( granted ) {
+                const { coords } = await getCurrentPositionAsync( {
+                    enableHighAccuracy: true,
+                } )
 
-            const { latitude, longitude } = coords
+                const { latitude, longitude } = coords
 
-            setCurrentRegion( {
-                latitude,
-                longitude,
-                latitudeDelta: 0.02,
-                longitudeDelta: 0.02,
-            } )
+                setCurrentRegion( {
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                } )
+            }
+            
         }
-        
+
         loadInitialPosition()
-    } , [ ] )
+    }, [ ] )
 
     if( !currentRegion ) {
         return null
     }
 
-    return < MapView  initialRegion={ currentRegion } style={ styles.map } />
+    return (
+        < MapView  initialRegion={ currentRegion } style={ styles.map } >
+            < Marker  coordinate={ { latitude: -23.5255981, longitude: -47.4919055 } }> 
+                <Image style={ styles.avatar } source={ { uri: 'https://avatars2.githubusercontent.com/u/56704254?s=400&v=4' } } />
+            </ Marker>
+        </ MapView>
+    )
 }
 
 const styles = StyleSheet.create( {
     map: {
         flex: 1
+    },
+    
+    avatar:{
+        width: 34,
+        height: 34,
+        borderRadius: 4,
+        borderWidth: 3,
+        borderColor: '#454687',
     }
 } )
 
