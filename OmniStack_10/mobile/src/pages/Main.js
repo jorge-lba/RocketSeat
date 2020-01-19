520a7e2fbadac996059c1f5da063e3810840f29a
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-18 23:53:29 -0300

    Add funcionalidade que atualiza o mapa quando um novo dev e cadastrado

--- OmniStack_10/mobile/src/pages/Main.js
+++ OmniStack_10/mobile/src/pages/Main.js
@@ -5,12 +5,25 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import api from '../services/api'
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket'
 
 function Main ( { navigation } ) {
     
     const [ devs, setDevs ] = useState( [] )
     const [ currentRegion, setCurrentRegion ] = useState( null )
     const [ techs, setTechs ] = useState( '' )
+
+    function setupWebSocket(){
+        disconnect()
+
+        const { latitude, longitude } = currentRegion
+
+        connect(
+            latitude,
+            longitude,
+            techs
+        )
+    }
 
     async function loadDevs() {
 
@@ -25,11 +38,16 @@ function Main ( { navigation } ) {
         } )
 
         setDevs( response.data.devs )
+        setupWebSocket( )
     }
 
     function handleRagionChanged( region ){
         setCurrentRegion( region )
     }
+
+    useEffect( ( ) => {
+        subscribeToNewDevs( dev => setDevs( [ ...devs, dev ] ) )
+    }, [devs] )
 
     useEffect( ( ) =>{
         
