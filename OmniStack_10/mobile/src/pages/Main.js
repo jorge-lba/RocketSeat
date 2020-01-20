a3858639cddd988ecc9191043628970b408bb412
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-20 01:25:18 -0300

    Add comando para o form subir quando o teclado for acionado

--- OmniStack_10/mobile/src/pages/Main.js
+++ OmniStack_10/mobile/src/pages/Main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
@@ -103,8 +103,8 @@ function Main ( { navigation } ) {
                 </ Marker>
                 ) ) }
             </ MapView>
-            
-            <View style={ styles.searchFrom }>
+            <KeyboardAvoidingView style={ styles.searchFrom } behavior='padding' keyboardVerticalOffset={95} >
+
                 <TextInput 
                     style={ styles.searchInput }
                     placeholder= 'Buscar devs por techs...'
@@ -117,8 +117,7 @@ function Main ( { navigation } ) {
                 <TouchableOpacity onPress={ loadDevs } style={ styles.loadButton }>
                     <MaterialIcons name="my-location" size={ 20 } color= "#FFF" />
                 </TouchableOpacity>
-            </View>
-             
+            </KeyboardAvoidingView>
         </>
     )
 }
@@ -155,16 +154,20 @@ const styles = StyleSheet.create( {
 
     searchFrom: {
         position:'absolute',
-        top: 20,
+        bottom: -20,
         left: 20,
         right: 20,
         zIndex: 5,
         flexDirection: 'row'
     },
-    form: {
-        flex: 1,
-        justifyContent: 'space-between',
-      },
+    searchFrom2: {
+        position:'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
     searchInput: {
         flex: 1,
         height: 50,
