798a16ef387b148e11f46ec2b613715912a9ba03
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-26 17:45:22 -0300

    Implementando o useState

--- OmniStack_11/frontend/src/App.js
+++ OmniStack_11/frontend/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header'
 
-
-
 function App() {
+
+  const [ counter, setCounter ] = useState( 0 )
+
+  function increment() {
+    setCounter( counter + 1 )
+  }
+
   return (
-    <Header>
-      Be The Hero
-    </Header>
+    <>
+      <Header>Contador: { counter }</Header>
+      <button onClick = { increment } >Incrimentar</button>
+    </>
   );
 }
 
