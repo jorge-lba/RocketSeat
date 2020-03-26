199a8a3bd2466571795371b71a5ae52e0ad2fc4e
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-25 23:19:29 -0300

    Add rota para deletar incidents

--- OmniStack_11/backend/src/routes.js
+++ OmniStack_11/backend/src/routes.js
@@ -1,11 +1,15 @@
 const express = require( 'express' )
 
 const OngController = require( './controlles/OngController' )
+const IncidentController = require( './controlles/IncidentController' )
 
 const routes = express.Router()
 
 routes.get( '/ongs', OngController.index )
-
 routes.post( '/ongs', OngController.create )
+
+routes.get( '/incidents', IncidentController.index )
+routes.post( '/incidents', IncidentController.create )
+routes.delete( '/incidents/:id', IncidentController.delete )
 
 module.exports = routes
