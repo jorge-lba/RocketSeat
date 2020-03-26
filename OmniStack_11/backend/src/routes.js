a6e0fec39b0042168c47c6043af7d8711ed98f7f
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-25 23:27:27 -0300

    Add rota para listar todos os casos de uma ong

--- OmniStack_11/backend/src/routes.js
+++ OmniStack_11/backend/src/routes.js
@@ -2,11 +2,15 @@ const express = require( 'express' )
 
 const OngController = require( './controlles/OngController' )
 const IncidentController = require( './controlles/IncidentController' )
+const ProfileController = require( './controlles/ProfileController' )
+
 
 const routes = express.Router()
 
 routes.get( '/ongs', OngController.index )
 routes.post( '/ongs', OngController.create )
+
+routes.get( '/profile', ProfileController.index )
 
 routes.get( '/incidents', IncidentController.index )
 routes.post( '/incidents', IncidentController.create )
