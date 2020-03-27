a0154ea824cfd7cbbfa0cde35bbe4b888d23bb13
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-27 20:57:38 -0300

    Add - Estilos para os cards de detalhes e contato do componente Detail

--- OmniStack_11/mobile/src/pages/Detail/index.js
+++ OmniStack_11/mobile/src/pages/Detail/index.js
@@ -16,7 +16,7 @@ function Detail() {
             </View>
 
             <View style={ styles.incident }>
-                <Text style={ styles.incidentProperty }>ONG:</Text>
+                <Text style={ [styles.incidentProperty, { marginTop: 0 }] }>ONG:</Text>
                 <Text style={ styles.incidentValue }>APAD</Text>
 
                 <Text style={ styles.incidentProperty }>CASO:</Text>
