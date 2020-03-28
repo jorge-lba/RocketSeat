7da38630630d838e7a66bec54a2496945dff4c9c
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-27 21:15:23 -0300

    Add- funcionalidade para enviar mensagem pelo whatsapp

--- OmniStack_11/mobile/src/pages/Detail/index.js
+++ OmniStack_11/mobile/src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
-import { View, Image, Text, TouchableOpacity } from 'react-native'
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import * as MailComposer from 'expo-mail-composer'
 
@@ -26,7 +26,7 @@ function Detail() {
     }
 
     function sendWhatsapp() {
-
+        Linking.openURL( `whatsapp://send?phone=5511985460185&text=${ message }` )
     }
 
     return (
@@ -57,7 +57,7 @@ function Detail() {
                 <Text style={ styles.heroDescription }>Entre em contato:</Text>
 
                 <View style={ styles.actions }>
-                    <TouchableOpacity style={ styles.action } onPress={ () => {} } >
+                    <TouchableOpacity style={ styles.action } onPress={ sendWhatsapp } >
                         <Text style={ styles.actionText }>Whatsapp</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={ styles.action } onPress={ sendEmail } >
