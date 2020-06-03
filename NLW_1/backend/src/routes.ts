986d0fa883aa43a5635692fa4614055af7b19cdf
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-06-03 00:17:33 -0300

    Add - Conceitos basicos de http e utilizaçao do insominia

--- NLW_1/backend/src/routes.ts
+++ NLW_1/backend/src/routes.ts
@@ -1,14 +1,39 @@
-import express from 'express'
+import express, { request, response } from 'express'
 import Twilio from './API/twilio'
 
 const routes = express.Router()
+const users = [
+    'Jorge',
+    'Karen',
+    'Diego',
+    'Ana',
+    'Pedro',
+    'Carlos'
+]
 
-routes.get('/', (request, response) => {
-    return response.json({
-        message:'Primeiro projeto da NLW RocketSeat!'
-    })
+routes.get('/users', (request, response) => {
+    const search = String(request.query.search)
+    const filteredUsers = search 
+        ? users.filter((user:string) => user.includes(search))
+        : users
+
+    return response.json(filteredUsers)
+})
+
+routes.get('/users/:id', (request, response) => {
+    const id:number = Number(request.params.id)
+    const user = users[id]
+
+    return response.json(user)
+})
+
+routes.post('/users', (request, response) => {
+    const user = request.body
+
+    return response.json(user)
 })
 
 routes.post('/twilio', Twilio.response )
+
 
 export default routes
