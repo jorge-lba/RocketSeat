3fdb6f858339900ab8f9790cd09f23254609b9cb
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-06-03 23:48:53 -0300

    Add - Rota para listar items

--- NLW_1/backend/src/app.ts
+++ NLW_1/backend/src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes'
-import bodyParser from 'body-parser'
+import path from 'path'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')) )
 
 export default app
--- NLW_1/backend/src/routes.ts
+++ NLW_1/backend/src/routes.ts
@@ -1,5 +1,6 @@
 import express, { request, response } from 'express'
 import Twilio from './API/twilio'
+import knex from './database/connection'
 
 const routes = express.Router()
 const users = [
@@ -11,8 +12,16 @@ const users = [
     'Carlos'
 ]
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello Wolrd!'})
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*')
+    const serializedItems = items.map(item => {
+        return{
+            title: item.title,
+            image_url: `${process.env.URL}/uploads/${item.image}`
+        }
+    })
+
+    return response.json(serializedItems)
 })
 
 routes.post('/twilio', Twilio.response )
