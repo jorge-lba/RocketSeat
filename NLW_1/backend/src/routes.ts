d343b06df8801e9f65cfc9911aaabfed385557df
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-06-04 01:10:31 -0300

    Add - Rotaque retorna um ponto de coleta

--- NLW_1/backend/src/routes.ts
+++ NLW_1/backend/src/routes.ts
@@ -1,69 +1,19 @@
 import express, { request, response } from 'express'
 import Twilio from './API/twilio'
-import knex from './database/connection'
-import Knex from 'knex'
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
 
 const routes = express.Router()
-const users = [
-    'Jorge',
-    'Karen',
-    'Diego',
-    'Ana',
-    'Pedro',
-    'Carlos'
-]
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*')
-    const serializedItems = items.map(item => {
-        return{
-            id: item.id,
-            title: item.title,
-            image_url: `${process.env.URL}/uploads/${item.image}`
-        }
-    })
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-    return response.json(serializedItems)
-})
+// indes, show, dreate, update, delete
+routes.get('/items', itemsController.index )
 
-routes.post('/points', async (request, response)=> {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    }= request.body
-
-    const trx = await knex.transaction()
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    })
-
-    const point_id = insertedIds[0]
-
-    const pointItems = items.map((item_id:number) => {
-        return{
-            item_id,
-            point_id
-        }
-    })
-
-    await trx('point_items').insert(pointItems)
-
-    return response.json({success: true})
-})
+routes.post('/points', pointsController.create )
+routes.get('/points/:id', pointsController.show )
 
 routes.post('/twilio', Twilio.response )
 
