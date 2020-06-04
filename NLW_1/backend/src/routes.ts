74abd2442e4b189a9eded7ade71478ff08ed9033
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-06-04 00:19:16 -0300

    Add - rota para cadastro de pontos de coleta

--- NLW_1/backend/src/routes.ts
+++ NLW_1/backend/src/routes.ts
@@ -1,6 +1,7 @@
 import express, { request, response } from 'express'
 import Twilio from './API/twilio'
 import knex from './database/connection'
+import Knex from 'knex'
 
 const routes = express.Router()
 const users = [
@@ -16,12 +17,52 @@ routes.get('/items', async (request, response) => {
     const items = await knex('items').select('*')
     const serializedItems = items.map(item => {
         return{
+            id: item.id,
             title: item.title,
             image_url: `${process.env.URL}/uploads/${item.image}`
         }
     })
 
     return response.json(serializedItems)
+})
+
+routes.post('/points', async (request, response)=> {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    }= request.body
+
+    const trx = await knex.transaction()
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    })
+
+    const point_id = insertedIds[0]
+
+    const pointItems = items.map((item_id:number) => {
+        return{
+            item_id,
+            point_id
+        }
+    })
+
+    await trx('point_items').insert(pointItems)
+
+    return response.json({success: true})
 })
 
 routes.post('/twilio', Twilio.response )
