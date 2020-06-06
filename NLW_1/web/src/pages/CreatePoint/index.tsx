cfdf4cdda22df2e53d5f9c4e49782c8afd03ea00
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-06-06 00:02:56 -0300

    Add - Integraçao do Mapa no formulario

--- NLW_1/web/src/pages/CreatePoint/index.tsx
+++ NLW_1/web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
+import {Map, TileLayer, Marker} from 'react-leaflet'
 
 import './styles.css'
 
@@ -62,6 +63,17 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
+                    <Map center={[-23.5263507, -47.4980909]} zoom={15} >
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-23.5263507, -47.4980909]}>
+
+                        </Marker>
+                    </Map>
+                    
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
