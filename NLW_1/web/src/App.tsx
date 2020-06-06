52ca8835cc8aeae284c4d5688f5cc5b9fed4d743
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-06-05 22:32:06 -0300

    Add - Contruçao da pagina home

--- NLW_1/web/src/App.tsx
+++ NLW_1/web/src/App.tsx
@@ -1,21 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header'
+import Home from './pages/Home'
 
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  function handleButtonClick(){
-    setCounter(counter+1)
-  }
-
   return (
-    <>
-      <Header title='Ecoleta' />
-  <h2>Contador: {counter}</h2>
-      <button type='button' onClick={handleButtonClick} >Aumentar</button>
-    </>
+      <div>
+        <Home />
+      </div>
   );
 }
 
