40ae4ae2b7fff4d92b41a04923c352d2cc0c994c
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-06-07 23:38:54 -0300

    Add - Funcionalidade para pegar geolocalizaçao do usuario

--- NLW_1/web/src/pages/CreatePoint/index.tsx
+++ NLW_1/web/src/pages/CreatePoint/index.tsx
@@ -29,11 +29,18 @@ const CreatePoint = () => {
     const [ufs, setUfs] = useState<string[]>([])
     const [citys, setCitys] = useState<string[]>([])
 
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0])
 
     const [selectedUf, setSelectedUf] = useState('0')
     const [selectedCity, setSelectedCity] = useState('0')
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0])
 
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const {latitude, longitude} = position.coords
+            setInitialPosition([latitude,longitude])
+        })
+    },[])
 
     useEffect(()=>{
         api.get('items').then(response => setItems(response.data))
@@ -129,7 +136,7 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
-                    <Map center={[-23.5263507, -47.4980909]} zoom={15} onClick={handleMapClick} >
+                    <Map center={initialPosition} zoom={15} onClick={handleMapClick} >
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
